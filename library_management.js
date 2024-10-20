1d911525788d228f26bc1fe624bb646de3119f2d
Brandon Mejia <brandonmejiabusiness@gmail.com>
2024-10-19 23:42:40 -0400

    Create Section class to manage books and availability

--- library_management.js
+++ library_management.js
@@ -18,3 +18,24 @@ class Book {
         this._isAvailable = status;
     }
 }
+
+//Task 2: Create a Section Class
+
+class section {
+    constructor(name) {
+        this.name = name;
+        this.books = []; //array to store books
+    }
+    addBook(book) {   //method to add book to section
+        this.books.push(book);
+    }
+    getAvailableBooks() {  //method to get total # of books
+        return this.books.filter(book => book.isAvailable).length;
+    }
+    listBooks () {  //method to list all books in section with availability
+        this.books.forEach(book => {
+            const availability = book.isAvailable ? "Available" : "Borrowed";
+            console.log("Title: ${book.title}, Availability: ${availability}");
+        });
+    }
+}
